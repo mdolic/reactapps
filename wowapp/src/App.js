d55b1ae8442e3b1488a8c5207c120532c52f3db9
mdolic <mirzetdolic2@gmail.com>
2018-12-25 15:36:56 -0600

    updated

--- wowapp/src/App.js
+++ wowapp/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ContactCard from './components/ContactCard';
 
 
 function App(){
@@ -9,6 +10,11 @@ function App(){
         <div>
            <Header/>
            <MainContent/>
+           <ContactCard
+                contact={{name:"whiskers",imgUrl:"https://www.petmd.com/sites/default/files/petmd-kitten-development.jpg",phone:"515-666-6663",email:"whiskerscat@gmail.com"
+                }}
+            />
+
            <Footer/>
 
         </div>
