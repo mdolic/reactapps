b11084d4f83baed2031f617ca2ef7b534ae20da8
mdolic <mirzetdolic2@gmail.com>
2018-12-25 11:15:38 -0600

    updated

---
+++ wowapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function App(){
+    return(
+        <div>
+            <input type="checkbox"/>
+            <p>todo item 1</p>
+
+            <input type="checkbox"/>
+            <p>todo item 2</p>
+
+            <input type="checkbox"/>
+            <p>todo item 3</p>
+        </div>
+    )
+}
+
+export default App;
--- wowapp/src/index.js
+++ wowapp/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-
-ReactDOM.render(<h1>hello mirzet</h1>, document.getElementById('root'));
-
-
-
+ReactDOM.render(<App/>, document.getElementById('root'));
