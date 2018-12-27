dc5fe184649c051f65b96d33402f70917309e66d
mdolic <mirzetdolic2@gmail.com>
2018-12-27 14:58:08 -0600

    refactor app.js

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import TodoItem from './components/TodoItem';
 import todoData from './todoData';
+import './css/style.css';
 
 class App extends React.Component{
     constructor(){
         super();
         this.state = {
-            todos: todoData // todoData loaded here
+            todos: todoData //this is an array of data from tododata.js
         }
     }
 
     render(){
-        const todoComponents = this.state.todos.map(item =>
-            <TodoItem key={item.id} todoitem={item}/>);
-    return(
-        <div className="todo-list">
-           {todoComponents}
-        </div>
-    )
-}
-}
+        const todosData = this.state.todos.map(item => <TodoItem key={item.id} todoitem={item}/>);
 
+        return (
+            <div className="todo-list">
+                {todosData}
+            </div>
+        )
+    }
+}
 export default App;
