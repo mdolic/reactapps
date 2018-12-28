92041ab9573fd7363ab44a14d76651a7dbbe40e2
mdolic <mirzetdolic2@gmail.com>
2018-12-28 09:54:12 -0600

    updated

--- reactpart2/src/App.js
+++ reactpart2/src/App.js
@@ -20,6 +20,8 @@ class App extends React.Component{
           isLoggedIn: true
       }
   }
+
+
     render(){
       let displayLoggedIn;
             if(this.state.isLoggedIn === true){
@@ -27,11 +29,20 @@ class App extends React.Component{
             }else{
             displayLoggedIn = 'Out';
             }
-
+        function handleClick(){
+            console.log('something was clicked');
+        }
+        function mouseOver(){
+            console.log("image was moused'over");
+        }
         return(
             <div>
                { /*<h1> Your are currently logged {this.state.isLoggedIn ? 'In' : 'Out'}</h1>*/}
                <h1> Your are currently logged {displayLoggedIn}</h1>
+                <img onMouseOver={mouseOver}src="https://blznav.akamaized.net/img/games/cards/card-world-of-warcraft-54576e6364584e35.jpg"/>
+               <br>
+
+               </br><button onClick={handleClick}>Click me!</button>
             </div>
         )
 
