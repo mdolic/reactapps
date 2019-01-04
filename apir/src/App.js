f9690975eef64c70df535ba1c7eaa61952e96c29
mdolic <mirzetdolic2@gmail.com>
2019-01-04 14:13:21 -0600

    updated

--- apir/src/App.js
+++ apir/src/App.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
 
 class App extends Component{
-
-
+    constructor(){
+        super();
+        this.state ={
+            unreadMessages: [
+                "call someone",
+                "another message"
+            ]
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h2>You have this {this.unreadMessages} unread Messages</h2>
+            </div>
+        )
+    }
 }
 export default App;
